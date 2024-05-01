f9895cb93aaf1f5fd1056330b8d91dac96d7325a
ujjwaltyagi2000 <ujjwaltyagi28@gmail.com>
2024-05-01 14:24:46 +0530

    all records from the API retrieved

--- api_test.py
+++ api_test.py
@@ -2,5 +2,5 @@
 import pandas as pd
 import requests
 
-df=pd.read_csv("https://api.data.gov.in/resource/8d3b6596-b09e-4077-aebf-425193185a5b?api-key=579b464db66ec23bdd0000014d331dc47176456852b5edabe3c24089&format=csv&offset=0&limit=999")
-df.to_csv('new_data.csv')
+df=pd.read_csv("https://api.data.gov.in/resource/8d3b6596-b09e-4077-aebf-425193185a5b?api-key=579b464db66ec23bdd0000014d331dc47176456852b5edabe3c24089&format=csv&offset=1500&limit=999")
+df.to_csv('no_data.csv')
