260dcd3af7fb6e2e7dbc75fd7f08503a61876c67
ujjwaltyagi2000 <ujjwaltyagi28@gmail.com>
2024-05-01 10:56:58 +0530

    offset and limit tested in API call

---
+++ api_test.py
@@ -0,0 +1,6 @@
+# testing code to fetch data from data.gov.in
+import pandas as pd
+import requests
+
+df=pd.read_csv("https://api.data.gov.in/resource/8d3b6596-b09e-4077-aebf-425193185a5b?api-key=579b464db66ec23bdd0000014d331dc47176456852b5edabe3c24089&format=csv&offset=0&limit=999")
+df.to_csv('new_data.csv')
